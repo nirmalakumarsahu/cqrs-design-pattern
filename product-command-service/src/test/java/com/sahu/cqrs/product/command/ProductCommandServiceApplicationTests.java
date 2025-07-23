c72c482905fe58fa8ddf75bc0e33a8f0f991fbdf
Nirmala Kumar Sahu <nirmalakumarsahu7@gmail.com>
2025-07-23 23:42:59 +0530

    Project structure is ready

---
+++ product-command-service/src/test/java/com/sahu/cqrs/product/command/ProductCommandServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahu.cqrs.product.command;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCommandServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ product-query-service/src/test/java/com/sahu/cqrs/product/query/ProductQueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahu.cqrs.product.query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductQueryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
